a84545d51fea870eec9a761a6114e19ba3a9136e
Sebastián Schambaher Gallegos <sebastianschambaher@gmail.com>
2021-10-18 21:26:40 -0500

    menu responsive modificacion archivo main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 const headerMenu = document.querySelector(".header-menu");
 
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".header");
+  const links = document.querySelectorAll(".header-navigation-link");
+  const headerTitle = document.querySelector(".header-title");
+
+  header.classList.toggle("sticky", window.scrollY > 0);
+  links.forEach(function (link) {
+    link.classList.toggle("sticky", window.scrollY > 0);
+  });
+  headerTitle.classList.toggle("sticky", window.scrollY > 0);
+});
+
 headerMenu.addEventListener("click", function () {
   const headerNavigation = document.querySelector(".header-navigation");
   headerNavigation.classList.toggle("show");
